b139d1f204517b77d69f5d60fd75fdd5fb60b14b
Alexey Nosov <nayretech@gmail.com>
2024-01-05 16:09:36 +0700

    Update VueEnt dependencies

--- src/vueent.ts
+++ src/vueent.ts
@@ -1,7 +1,8 @@
+import { onBeforeMount, onMounted, onBeforeUnmount, onUnmounted } from 'vue';
 import { initVueent } from '@vueent/core';
 
 /**
  * Initialized VueEnt core instance.
  */
 export const { useVueent, registerService, registerController, useService, useController, injectService, injectController } =
-  initVueent();
+  initVueent({ onBeforeMount, onMounted, onBeforeUnmount, onUnmounted });
